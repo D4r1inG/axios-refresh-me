ba40fd927708551db8cca2eb01af5b43fbf3be55
quannt86 <quannt86>
2024-10-03 15:38:12 +0700

    Add minify config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'tsup';
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  entry: ['src', '!src/**/*.spec.*', '!src/**/*.test.*', '!src/**/*.mock.*'],
+  entry: ['src'],
   clean: true,
   platform: 'node',
   format: 'esm',
   target: 'esnext',
   dts: true,
   treeshake: true,
-  sourcemap: true,
   bundle: false,
+  minify: true,
 });
